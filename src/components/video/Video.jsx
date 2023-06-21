f8a93976af799350d814384cff5a9f4e086f6238
mason park <joung4342@gmail.com>
2023-06-21 20:32:22 +0900

    본문작업_self

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
-function Video(props) {
+function Video({id,snippet}) {
     return (
-        <div></div>
+        <div>
+            <iframe
+                src={`https://www.youtube.com/embed/${id}`}
+            />
+            <h2>{snippet.title}</h2>
+            <h3>{snippet.channelTitle}</h3>
+            <h3>{snippet.publishedAt}</h3>
+        </div>
     );
 }
 
