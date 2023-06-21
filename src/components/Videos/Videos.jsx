234161b129363e81dd6a8f46fb1fe1e5e6a1a3d5
mason park <joung4342@gmail.com>
2023-06-21 20:32:14 +0900

    본문작업_self

--- src/components/Videos/Videos.jsx
+++ src/components/Videos/Videos.jsx
@@ -1,23 +1,36 @@
-import React, {useEffect} from 'react';
-import {logDOM} from "@testing-library/react";
+import React, {useEffect, useState} from 'react';
+import Video from "../video/Video";
+import './videos.css';
+
 function Videos(props) {
-    useEffect(()=>{
+    const [videos, setVideos] = useState([]);
+    useEffect(() => {
         var requestOptions = {
             method: 'GET',
             redirect: 'follow'
         };
-
-        fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&hl=ko&maxResults=50&key=${process.env.REACT_APP_YOUTUBE_KEY}`, requestOptions)
+        fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&hl=ko&maxResults=25&key=${process.env.REACT_APP_YOUTUBE_KEY}`, requestOptions)
             .then(response => response.json())
-            .then(res => console.log(res));
-    },[])
+            .then(res => setVideos(res.items));
+    }, []);
+
     return (
+        <main>
         <ul className='videos-list'>
-            <li className='video'>hi</li>
+            {
+                videos.map(video => (
+                    <li key={video.id} className='video-container'>
+                        <Video
+                            id={video.id}
+                            snippet={video.snippet}
+                        />
+                    </li>
+                ))
+            }
         </ul>
+        </main>
     );
 }
-
 
 
 export default Videos;
--- src/components/service/youtubeUtil.js
+++ src/components/service/youtubeUtil.js
@@ -8,7 +8,7 @@ export  default class YoutubeUtil {
     }
 
      async getPopularVideoList() {
-       const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&hl=ko&maxResults=50&key=${this.API_KEY}`, this.requestOptions);
+       const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&hl=ko&maxResults=25&key=${this.API_KEY}`, this.requestOptions);
          return await response.json();
     }
 }
