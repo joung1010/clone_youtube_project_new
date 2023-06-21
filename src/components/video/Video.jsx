6f527e4ce3ad3f62b3fe3325f51a09d02419e85f
mason park <joung4342@gmail.com>
2023-06-21 21:08:18 +0900

    본문작업2_self

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import './video.css'
 
 function Video({id,snippet}) {
     return (
-        <div>
+        <div className='video'>
             <iframe
+                className='video-item'
                 src={`https://www.youtube.com/embed/${id}`}
             />
-            <h2>{snippet.title}</h2>
-            <h3>{snippet.channelTitle}</h3>
-            <h3>{snippet.publishedAt}</h3>
+            <h2 className='video-title'>{snippet.title}</h2>
+            <h3 className='video-channel'>{snippet.channelTitle}</h3>
+            <h3 className='video-upload'>{snippet.publishedAt}</h3>
         </div>
     );
 }
