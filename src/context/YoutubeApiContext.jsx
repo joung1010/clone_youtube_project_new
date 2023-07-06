5dbd83febb1dc3d824bb4d249ce87409a06ad9a4
mason park <joung4342@gmail.com>
2023-07-06 19:38:01 +0900

    Videos component 개선6 (DI 처리 Context 리팩토링)

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -1,9 +1,13 @@
 import {createContext, useContext} from "react";
 import Youtube from "../service/api/youtube";
+import YoutubeClient from "../service/api/youtubeClient";
+import TestYoutubeClient from "../service/api/testYoutubeClient";
 
 export const YoutubeApiContext = createContext();
 
-const youtube = new Youtube();
+const client = new YoutubeClient();
+// const client = new TestYoutubeClient();
+const youtube = new Youtube(client);
 export function YoutubeApiProvider({children}) {
     return (
         <YoutubeApiContext.Provider value={{youtube}}>
--- src/service/api/youtube.js
+++ src/service/api/youtube.js
@@ -1,13 +1,6 @@
-import axios from "axios";
-
 export default class Youtube {
-    constructor() {
-        this.httpClient = axios.create({
-            baseURL: 'https://youtube.googleapis.com/youtube/v3/',
-            params: {
-                key: process.env.REACT_APP_YOUTUBE_KEY
-            }
-        });
+    constructor(apiClient) {
+        this.apiClient = apiClient;
     }
 
     async search(keyword) {
@@ -15,29 +8,27 @@ export default class Youtube {
     };
 
     async #searchByKeyword(keyword) {
-        return this.httpClient
-            .get('search', {
-                params: {
-                    part: 'snippet',
-                    maxResults: 25,
-                    type:'video',
-                    q: keyword,
-                }
-            })
+        return this.apiClient.search( {
+            params: {
+                part: 'snippet',
+                maxResults: 25,
+                type:'video',
+                q: keyword,
+            }
+        })
             .then((res) => res.data.items)
             .then(items => items.map(item => ({...item, id: item.id.videoId})));
     }
 
     async #mostPopular() {
-        return this.httpClient
-            .get('videos', {
-                params: {
-                    part: 'snippet',
-                    chart: 'mostPopular',
-                    hl: 'ko',
-                    maxResults: 25,
-                }
-            })
+        return this.apiClient.videos( {
+            params: {
+                part: 'snippet',
+                chart: 'mostPopular',
+                hl: 'ko',
+                maxResults: 25,
+            }
+        })
             .then((res) => res.data.items);
     }
 }
