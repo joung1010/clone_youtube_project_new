2ef3028787703753709d4f474332f1acdb408c63
mason park <joung4342@gmail.com>
2023-07-19 20:55:05 +0900

    useLocation 이용하여 객체값을 다른 component에 전달하기

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './video.css'
 import {formatAgo} from "../../util/date";
+import {useNavigate} from "react-router-dom";
 
 function Video({id, videos: {snippet}}) {
     const {title, thumbnails, channelTitle, publishedAt} = snippet;
+    const navigate = useNavigate();
+
+    const handleOnclick = (e) => {
+        e.preventDefault();
+        navigate(`/videos/watch/${id}`,{state: snippet});
+    };
     return (
-        <li key={id} >
+        <li key={id}  onClick={handleOnclick}>
             <img className='video-img' src={thumbnails.medium.url} alt={title}/>
             <div>
                 <p className='video-title'>{title}</p>
@@ -14,12 +21,6 @@ function Video({id, videos: {snippet}}) {
             </div>
         </li>
     );
-}
-
-{/*<iframe
-                className='video-item'
-                src={`https://www.youtube.com/embed/${id}`}
-            />*/
 }
 
 export default Video;
--- src/page/videoDetail/VideoDetail.jsx
+++ src/page/videoDetail/VideoDetail.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import {useLocation, useParams} from "react-router-dom";
 
 function VideoDetail(props) {
+    const {videoId} = useParams();
+    const {state} = useLocation();
+    console.log(state);
     return (
-        <div>Detail</div>
+        <div>
+            <div>
+                <iframe
+                    className='video-item'
+                    src={`https://www.youtube.com/embed/${videoId}`}
+                />
+            </div>
+            <div>
+                List
+            </div>
+        </div>
     );
 }
 
